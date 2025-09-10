416908575a410691eccb8bbf74c6eda768332de6
Soulé Soumaré <sourtoumo@gmail.com>
2025-09-10 12:21:22 +0200

    delete /api folder and add .vercelignore file

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-// Import the compiled Express app
-const app = require(path.join(__dirname, '..', 'dist', 'index.js'));
-
-// Export as serverless function
-module.exports = app.default || app;
